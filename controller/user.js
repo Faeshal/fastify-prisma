c80e77ce0c80057935a8b38d6fc0e659dd44d2f0
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2021-11-20 17:25:36 +0700

    build crud

--- controller/user.js
+++ controller/user.js
@@ -1,19 +1,54 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-// * @route   GET api/users
+// * @route   GET /api/v1/users
 // @desc      Get all users
 // @access    Private
 exports.getUsers = async (request, reply) => {
-  const data = await prisma.user.findMany();
+  const data = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
   reply.code(200).send({ success: true, data });
 };
 
-// * @route   POST api/users
+// * @route   POST /api/v1/users
 // @desc      Create new users
 // @access    Private
 exports.createUser = async (request, reply) => {
   const { name, email, phone } = request.body;
   const result = await prisma.user.create({ data: { name, email, phone } });
+  reply.code(200).send({ success: true, data: result });
+};
+
+// * @route   POST /api/v1/users/:id
+// @desc      get detail user
+// @access    Private
+exports.getUser = async (request, reply) => {
+  const { id } = request.params;
+  const result = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+  reply.code(200).send({ success: true, data: result || {} });
+};
+
+// * @route   POST /api/v1/users/:id
+// @desc      update user
+// @access    Private
+exports.updateUser = async (request, reply) => {
+  const { id } = request.params;
+  const result = await prisma.user.update({
+    where: { id: parseInt(id) },
+    data: request.body,
+  });
+  reply.code(200).send({ success: true, data: result });
+};
+
+// * @route   POST /api/v1/users/:id
+// @desc      delete user
+// @access    Private
+exports.deleteuser = async (request, reply) => {
+  const { id } = request.params;
+  const result = await prisma.user.delete({ where: { id: parseInt(id) } });
   reply.code(200).send({ success: true, data: result });
 };
--- route/user.js
+++ route/user.js
@@ -1,7 +1,11 @@
 const userController = require("../controller/user");
+
 async function routes(fastify, options) {
   fastify.get("/api/v1/users", userController.getUsers);
   fastify.post("/api/v1/users", userController.createUser);
+  fastify.get("/api/v1/users/:id", userController.getUser);
+  fastify.put("/api/v1/users/:id", userController.updateUser);
+  fastify.delete("/api/v1/users/:id", userController.deleteuser);
 }
 
 module.exports = routes;
